706125c34a65fcff25d62c480fe4026d51b1125e
Chinmay Pendharkar <notthetup@gmail.com>
2015-12-27 18:27:42 +0800

    fixed linting and cs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,38 +1,41 @@
+
 (function(document) {
-  'use strict';
+	'use strict';
+
 	window.addEventListener('WebComponentsReady', function() {
-    console.log('WebComponents Ready!');
+		console.log('WebComponents Ready!');
 	});
-  window.addEventListener('load', function(){
-    var buttons = document.querySelectorAll('sound-button');
 
-    document.addEventListener('keydown', keydownListener);
-    document.addEventListener('keyup', keyUpListener);
+	window.addEventListener('load', function() {
+		var buttons = document.querySelectorAll('sound-button');
 
-    function keydownListener(evt){
-      if (document.activeElement.tagName !== "BODY"){
-        return;
-      }
-      for (var index = 0; index < buttons.length; index++){
-        var thisButton = buttons[index];
-        if (thisButton.keyCode === evt.keyCode && (
-          thisButton.buttonState === 'up' ||
-            thisButton.buttonState === 'init')){
-          thisButton.buttonState = 'down';
-        }
-      }
-    }
+		document.addEventListener('keydown', keyDownListener);
+		document.addEventListener('keyup', keyUpListener);
 
-    function keyUpListener(evt){
-      if (document.activeElement.tagName !== "BODY"){
-        return;
-      }
-      for (var index = 0; index < buttons.length; index++){
-        var thisButton = buttons[index];
-        if (thisButton.keyCode === evt.keyCode && thisButton.buttonState === 'down'){
-          thisButton.buttonState = 'up';
-        }
-      }
-    }
-  });
-})(document)
+		function keyDownListener(evt) {
+			if (document.activeElement.tagName !== 'BODY') {
+				return;
+			}
+			for (var index = 0; index < buttons.length; index++) {
+				var thisButton = buttons[index];
+				if (thisButton.keyCode === evt.keyCode &&
+					(thisButton.buttonState === 'up' ||
+						thisButton.buttonState === 'init')) {
+					thisButton.buttonState = 'down';
+				}
+			}
+		}
+
+		function keyUpListener(evt) {
+			if (document.activeElement.tagName !== 'BODY') {
+				return;
+			}
+			for (var index = 0; index < buttons.length; index++) {
+				var thisButton = buttons[index];
+				if (thisButton.keyCode === evt.keyCode && thisButton.buttonState === 'down') {
+					thisButton.buttonState = 'up';
+				}
+			}
+		}
+	});
+})(document);
